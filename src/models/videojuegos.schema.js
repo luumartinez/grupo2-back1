726979630d722b29bcf17cb648e5a9b669be7c23
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-09-17 18:39:56 -0300

    agregamos campos adicionales al esquema

--- src/models/videojuegos.schema.js
+++ src/models/videojuegos.schema.js
@@ -20,6 +20,52 @@ const VideojuegoSchema = new Schema({
     required: true,
     min: [0, "El precio no puede ser negativo"],
     max: [999999, "El precio no puede ser mayor a 999999"],
-  }
+  },
+  imagen: {
+    type: String,
+    default: "",
+  },
+  trailer: {
+    type: String,
+    default: "",
+    trim: true,
+  },
+  plataformas: {
+    type: [String],
+    enum: [
+      "PC",
+      "PlayStation 4",
+      "Xbox One",
+      "Nintendo Switch",
+      "PlayStation 5",
+      "Xbox Series X",
+      "PlayStation 3",
+      "Xbox 360",
+    ],
+  },
+  genero: {
+    type: [String],
+    enum: ["Acción", "Aventura", "Estrategia", "Deportes", "Simulación"],
+    required: true,
+  },
+  fechaLanzamiento: {
+    type: Date,
+    required: true,
+  },
+  version: {
+    type: String,
+    trim: true,
+  },
+  calificacion: {
+    type: Number,
+    min: [0, "Calificación mínima es 0"],
+    max: [10, "Calificación máxima es 10"],
+  },
+  habilitado: {
+    type: Boolean,
+    default: true,
+  },
 });
+
+
 
