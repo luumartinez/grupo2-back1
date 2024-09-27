82de3725bdb2bb81017f67245477e8e2822f4db9
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-09-27 11:22:18 -0300

    Mejora de mensajes de validación en las rutas de usuarios

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,66 +1,70 @@
-const serviceUsuario = require('../services/usuarios.sevices')
-const { validationResult } = require('express-validator')
+const serviceUsuario = require("../services/usuarios.sevices");
+const { validationResult } = require("express-validator");
 
 const registrarUsuario = async (req, res) => {
   try {
-    const { errors } = validationResult(req)
+    const { errors } = validationResult(req);
 
     if (errors.length) {
-      return res.status(400).json({ msg: errors[0].msg })
+      return res.status(400).json({ msg: errors[0].msg });
     }
 
-    const result = await serviceUsuario.nuevoUsuario(req.body)
+    const result = await serviceUsuario.nuevoUsuario(req.body);
     if (result === 201) {
-      res.status(201).json({ msg: 'Usuario registrado con exito' })
-    }else if(result === 409){
-      res.status(409).json({msg:'Error al crear: Rol incorrecto. Solo se puede ser usuario o admin'})
+      res.status(201).json({ msg: result.msg });
+    } else if (result === 400) {
+      res.status(400).json({ msg: result.msg });
+    } else {
+      res.status(500).json({ msg: result.msg });
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 const iniciarSesionUsuario = async (req, res) => {
   try {
-    const result = await serviceUsuario.inicioSesion(req.body)
+    const result = await serviceUsuario.inicioSesion(req.body);
 
     if (result.code === 400) {
-      res.status(400).json({ msg: 'Usuario y/o contraseña incorrecto' })
-    } else {
-      res.status(200).json({ msg: 'Usuario inicio sesion', token: result.token })
+      res.status(400).json({ msg: result.msg });
+    } else if (result.code === 200){
+      res.status(200).json({ msg: result.msg });
+    }else {
+      res.status(500).json({ msg: result.msg });
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 const obtenerTodosLosUsuarios = async (req, res) => {
-  try {
-    const usuarios = await serviceUsuario.obtenerTodosLosUsuarios()
-    res.status(200).json(usuarios)
-  } catch (error) {
-    console.log(error)
+  const result = await serviceUsuario.obtenerTodosLosUsuarios();
+
+  if (result.statusCode === 200) {
+    res.status(200).json({ usuarios: result.usuarios, msg: result.msg });
+  } else if (result.statusCode === 404) {
+    res.status(404).json({ msg: result.msg });
+  } else {
+    res.status(500).json({ msg: result.msg });
   }
-}
+};
 
 const obtenerUnUsuario = async (req, res) => {
-  try {
-    const { errors } = validationResult(req)
-    
-    if (errors.length) {
-      return res.status(400).json({ msg: errors[0].msg })
-    }
-    
-    const usuario = await serviceUsuario.obtenerUnUsuario(req.params.idUsuario)
-    res.status(200).json({ msg: 'Usuario encontrado', usuario })
-  } catch (error) {
-    console.log(error)
+  const result = await serviceUsuario.obtenerUnUsuario(req.params.idUsuario);
+
+  if (result.statusCode === 200) {
+    res.status(200).json({ usuario: result.usuario, msg: result.msg });
+  } else if (result.statusCode === 404) {
+    res.status(404).json({ msg: result.msg });
+  } else {
+    res.status(500).json({ msg: result.msg });
   }
-}
+};
 
 const eliminarUnUsuario = async (req, res) => {
   const result = await serviceUsuario.eliminarUsuario(req.params.idUsuario);
-  
+
   if (result.statusCode === 200) {
     res.status(200).json({ msg: result.msg, usuarios: result.usuarios });
   } else if (result.statusCode === 404) {
@@ -68,33 +72,33 @@ const eliminarUnUsuario = async (req, res) => {
   } else {
     res.status(500).json({ msg: result.msg });
   }
-}
-
+};
 
 const editarUsuario = async (req, res) => {
-  const result = await serviceUsuario.actualizarUsuario(req.params.idUsuario, req.body)
-  
-  if(result.statusCode === 200){
-    res.status(200).json({msg: result.msg})
-   }else if (result.statusCode === 404) {
+  const result = await serviceUsuario.actualizarUsuario(
+    req.params.idUsuario,
+    req.body
+  );
+
+  if (result.statusCode === 200) {
+    res.status(200).json({ msg: result.msg });
+  } else if (result.statusCode === 404) {
     res.status(404).json({ msg: result.msg });
-   }
-   else{
-    res.status(500).json({msg: result.msg})
-   }
-}
+  } else {
+    res.status(500).json({ msg: result.msg });
+  }
+};
 
+const cambiarEstadoUsuario = async (req, res) => {
+  const { idUsuario, accion } = req.params;
 
-const cambiarEstadoUsuario= async (req, res) => {
-  const { idUsuario, accion } = req.params; 
-  
   let result;
-  if (accion === 'habilitar') {
+  if (accion === "habilitar") {
     result = await serviceUsuario.habilitarUsuario(idUsuario);
-  } else if (accion === 'deshabilitar') {
+  } else if (accion === "deshabilitar") {
     result = await serviceUsuario.deshabilitarUsuario(idUsuario);
   } else {
-    return res.status(400).json({ msg: 'Acción no válida' });
+    return res.status(400).json({ msg: "Acción no válida" });
   }
 
   if (result.statusCode === 200) {
@@ -104,15 +108,17 @@ const cambiarEstadoUsuario= async (req, res) => {
   }
 };
 
-
 const agregarFotoPerfil = async (req, res) => {
-  const result =  await serviceUsuario.fotoPerfil(req.params.idUsuario, req.file)
-  if(result.statusCode === 200){
-    res.status(200).json({msg: result.msg})
-   }else{
-    res.status(500).json({msg: result.msg})
-   }
-}
+  const result = await serviceUsuario.fotoPerfil(
+    req.params.idUsuario,
+    req.file
+  );
+  if (result.statusCode === 200) {
+    res.status(200).json({ msg: result.msg });
+  } else {
+    res.status(500).json({ msg: result.msg });
+  }
+};
 
 module.exports = {
   registrarUsuario,
@@ -122,5 +128,5 @@ module.exports = {
   editarUsuario,
   eliminarUnUsuario,
   cambiarEstadoUsuario,
-  agregarFotoPerfil 
-}
+  agregarFotoPerfil,
+};
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -17,27 +17,60 @@ const multer = require("../middlewares/multer");
 router.post(
   "/",
   [
-    check("nombreUsuario", "Campo USUARIO esta vacio").not().isEmpty(),
-    check("nombreUsuario", "min:5 caracteres y max: 40 caracteres").isLength({
-      min: 5,
-      max: 40,
-    }),
-    check("contrasenia", "Campo CONTRASEÑA esta vacio").not().isEmpty(),
-    check("contrasenia", "min: 8 caracteres y max: 50 caracteres").isLength({
-      min: 8,
-      max: 50,
-    }),
+    check("nombreUsuario")
+      .not()
+      .isEmpty()
+      .withMessage("El campo 'Usuario' no puede estar vacío.")
+      .isLength({ min: 5, max: 40 })
+      .withMessage("El 'Usuario' debe tener entre 5 y 40 caracteres."),
+
+    check("contrasenia")
+      .not()
+      .isEmpty()
+      .withMessage("El campo 'Contraseña' no puede estar vacío.")
+      .isLength({ min: 8, max: 50 })
+      .withMessage("La 'Contraseña' debe tener entre 8 y 50 caracteres."),
+
+      check("nombre")
+      .not()
+      .isEmpty()
+      .withMessage("El campo 'nombre' no puede estar vacío.")
+      .isLength({ min:3, max: 25 })
+      .withMessage("El 'Nombre' debe tener entre 3 y 25 caracteres."),  
+
+      check("apellido")
+      .not()
+      .isEmpty()
+      .withMessage("El campo 'Contraseña' no puede estar vacío.")
+      .isLength({ min: 3, max: 25 })
+      .withMessage("El 'Apellido' debe tener entre 3 y 25 caracteres."),
+
+      check("email")
+      .not()
+      .isEmpty()
+      .withMessage("El campo 'Contraseña' no puede estar vacío.")
+      .isLength({ min: 10 })
+      .withMessage("La 'Contraseña' debe tener como minimo 10 caracteres."),
   ],
   registrarUsuario
 );
+
 router.post(
   "/login",
   [
-    check("nombreUsuario", "Campo USUARIO esta vacio").not().isEmpty(),
-    check("contrasenia", "Campo CONTRASEÑA esta vacio").not().isEmpty(),
+    check("nombreUsuario")
+      .not()
+      .isEmpty()
+      .withMessage("El campo 'Usuario' no puede estar vacío."),
+
+    check("contrasenia")
+      .not()
+      .isEmpty()
+      .withMessage("El campo 'Contraseña' no puede estar vacío."),
   ],
   iniciarSesionUsuario
 );
+
 router.post(
   "/fotoPerfil/:idUsuario",
   multer.single("foto"),
@@ -46,16 +79,10 @@ router.post(
 );
 
 router.get("/", auth("admin"), obtenerTodosLosUsuarios);
-router.get(
-  "/:idUsuario",
-  [
-    /*   check('_id', 'Formato ID incorrecto').isMongoId() */
-  ],
-  auth("admin"),
-  obtenerUnUsuario
-);
-router.delete("/:idUsuario", auth("admin"), eliminarUnUsuario);
+router.get("/:idUsuario", auth("admin"), obtenerUnUsuario);
+
 router.put("/:idUsuario/:accion", auth("admin"), cambiarEstadoUsuario);
 router.put("/:idUsuario", auth("admin"), editarUsuario);
+router.delete("/:idUsuario", auth("admin"), eliminarUnUsuario);
 
 module.exports = router;
